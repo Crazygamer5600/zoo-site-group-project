3562a03ebdb6db2a0ee737298144570739e8b424
orionvee <167953452+orionvee@users.noreply.github.com>
2025-09-09 20:11:12 +0930

    more animals Query

---
+++ More_Animals.sql
@@ -0,0 +1,109 @@
+INSERT INTO Animals (name, species, description, extraInfo, category, animalPhoto, animalLocation) VALUES
+
+('Rocky', 'Ursus arctos', 'Brown bear with thick fur', 'Enjoys fishing in streams', 'Mammal', 'rocky.jpg', 'Bear Den'),
+
+('Splash', 'Pongo pygmaeus', 'Playful orangutan with reddish hair', 'Loves swinging on ropes', 'Mammal', 'splash.jpg', 'Primate House'),
+
+('Glide', 'Pteropus vampyrus', 'Large flying fox bat', 'Active during the night', 'Mammal', 'glide.jpg', 'Nocturnal House'),
+
+('Spike', 'Ceratotherium simum', 'White rhinoceros with a broad mouth', 'Weighs over 2 tons', 'Mammal', 'spike.jpg', 'Savannah Habitat'),
+
+('Spotty', 'Giraffa camelopardalis', 'Giraffe with distinctive long neck', 'Feeds on acacia leaves', 'Mammal', 'spotty.jpg', 'Savannah Habitat'),
+
+('Grumpy', 'Crocodylus niloticus', 'Nile crocodile with powerful jaws', 'Spends time basking on riverbanks', 'Reptile', 'grumpy.jpg', 'Reptile House'),
+
+('Slither', 'Naja naja', 'Indian cobra with hood pattern', 'Venomous and revered in culture', 'Reptile', 'slither.jpg', 'Reptile House'),
+
+('Pebbles', 'Chelonia mydas', 'Green sea turtle with smooth shell', 'Migrates long distances', 'Reptile', 'pebbles.jpg', 'Aquatic Zone'),
+
+('Hopper', 'Macropus rufus', 'Red kangaroo, powerful jumper', 'Largest marsupial in the world', 'Mammal', 'hopper.jpg', 'Australia Zone'),
+
+('Joey', 'Phascolarctos cinereus', 'Koala, grey and fluffy', 'Eats eucalyptus leaves daily', 'Mammal', 'joey.jpg', 'Australia Zone'),
+
+('Chilly', 'Spheniscus demersus', 'African penguin with black markings', 'Enjoys swimming in cold water', 'Bird', 'chilly.jpg', 'Penguin Habitat'),
+
+('Fluffy', 'Struthio camelus', 'Ostrich, world’s largest bird', 'Can run up to 70 km/h', 'Bird', 'fluffy.jpg', 'Savannah Habitat'),
+
+('Ranger', 'Equus zebra', 'Plains zebra with bold stripes', 'Moves in herds', 'Mammal', 'ranger.jpg', 'Savannah Habitat'),
+
+('Pogo', 'Suricata suricatta', 'Meerkat standing on hind legs', 'Lives in colonies', 'Mammal', 'pogo.jpg', 'Desert Exhibit'),
+
+('Dash', 'Acinonyx jubatus', 'Cheetah, fast sprinter', 'Runs up to 120 km/h', 'Mammal', 'dash.jpg', 'Savannah Habitat'),
+
+('Bamboo', 'Ailuropoda melanoleuca', 'Giant panda, black and white', 'Eats mostly bamboo', 'Mammal', 'bamboo.jpg', 'Asia Habitat'),
+
+('Misty', 'Ailuropoda melanoleuca qinlingensis', 'Qinling panda with brownish fur', 'Rare panda subspecies', 'Mammal', 'misty.jpg', 'Asia Habitat'),
+
+('Snowy', 'Panthera uncia', 'Snow leopard with thick spotted coat', 'Lives in cold mountains', 'Mammal', 'snowy.jpg', 'Mountain Zone'),
+
+('Shadow', 'Canis lupus arctos', 'Arctic wolf, white fur coat', 'Pack hunter', 'Mammal', 'shadow.jpg', 'Tundra Zone'),
+
+('Echo', 'Hylobates lar', 'Lar gibbon, agile tree-dweller', 'Communicates with loud calls', 'Mammal', 'echo.jpg', 'Primate House'),
+
+('Rusty', 'Leptailurus serval', 'Serval cat with large ears', 'Excellent hunter of small prey', 'Mammal', 'rusty.jpg', 'Savannah Habitat'),
+
+('Gusty', 'Phoenicopterus roseus', 'Flamingo with pink plumage', 'Stands on one leg often', 'Bird', 'gusty.jpg', 'Wetlands Exhibit'),
+
+('Trunkie', 'Tapirus terrestris', 'Brazilian tapir with short snout', 'Enjoys swimming in rivers', 'Mammal', 'trunkie.jpg', 'Rainforest Habitat'),
+
+('Prickle', 'Echidna aculeatus', 'Short-beaked echidna with spines', 'Feeds on ants and termites', 'Mammal', 'prickle.jpg', 'Australia Zone'),
+
+('Bounce', 'Dendrolagus goodfellowi', 'Goodfellow’s tree kangaroo', 'Climbs trees with ease', 'Mammal', 'bounce.jpg', 'Australia Zone'),
+
+('Quill', 'Hystrix africaeaustralis', 'African crested porcupine', 'Raises quills when threatened', 'Mammal', 'quill.jpg', 'Nocturnal House'),
+
+('Nibbles', 'Castor canadensis', 'North American beaver', 'Builds lodges with wood', 'Mammal', 'nibbles.jpg', 'River Exhibit'),
+
+('Blubber', 'Odobenus rosmarus', 'Walrus with long tusks', 'Loves to rest on ice', 'Mammal', 'blubber.jpg', 'Aquatic Zone'),
+
+('Whiskers', 'Enhydra lutris', 'Sea otter floating on back', 'Uses tools to crack shells', 'Mammal', 'whiskers.jpg', 'Aquatic Zone'),
+
+('Spikelet', 'Sus scrofa', 'Wild boar with coarse hair', 'Omnivorous feeder', 'Mammal', 'spikelet.jpg', 'Forest Habitat'),
+
+('Rolo', 'Capra ibex', 'Alpine ibex with curved horns', 'Skilled mountain climber', 'Mammal', 'rolo.jpg', 'Mountain Zone'),
+
+('Storm', 'Haliaeetus leucocephalus', 'Bald eagle with strong beak', 'National bird of USA', 'Bird', 'storm.jpg', 'Aviary Zone'),
+
+('Twilight', 'Tyto alba', 'Barn owl with heart-shaped face', 'Hunts silently at night', 'Bird', 'twilight.jpg', 'Nocturnal House'),
+
+('Cuddles', 'Lemur catta', 'Ring-tailed lemur with striped tail', 'Highly social primate', 'Mammal', 'cuddles.jpg', 'Madagascar Zone'),
+
+('Zippy', 'Hippopotamus amphibius', 'Hippo with barrel-shaped body', 'Spends hours in water', 'Mammal', 'zippy.jpg', 'River Exhibit'),
+
+('Claws', 'Panthera pardus', 'Leopard, spotted coat', 'Excellent climber', 'Mammal', 'claws.jpg', 'Jungle Habitat'),
+
+('Ruffles', 'Meleagris gallopavo', 'Wild turkey with fan tail', 'Recognizable gobbling sound', 'Bird', 'ruffles.jpg', 'Farm Exhibit'),
+
+('Bongo', 'Tragelaphus eurycerus', 'Bongo antelope with striking stripes', 'Shy forest dweller', 'Mammal', 'bongo.jpg', 'Forest Habitat'),
+
+('Munch', 'Okapia johnstoni', 'Okapi with zebra-like legs', 'Closely related to giraffe', 'Mammal', 'munch.jpg', 'Rainforest Habitat'),
+
+('Rivera', 'Hydrochoerus hydrochaeris', 'Capybara, giant rodent', 'Lives near rivers', 'Mammal', 'rivera.jpg', 'Wetlands Exhibit'),
+
+('Pebble', 'Pygocentrus nattereri', 'Red-bellied piranha', 'Sharp-toothed fish', 'Fish', 'pebble.jpg', 'Aquatic Zone'),
+
+('Finny', 'Sphyrna lewini', 'Hammerhead shark', 'Distinctive head shape', 'Fish', 'finny.jpg', 'Shark Tank'),
+
+('Marble', 'Hippocampus kuda', 'Yellow seahorse', 'Clings to sea grasses', 'Fish', 'marble.jpg', 'Aquatic Zone'),
+
+('Starry', 'Astropecten articulatus', 'Starfish with five arms', 'Regenerates lost limbs', 'Invertebrate', 'starry.jpg', 'Aquatic Zone'),
+
+('Jewel', 'Papilio ulysses', 'Blue Ulysses butterfly', 'Tropical rainforest dweller', 'Insect', 'jewel.jpg', 'Insectarium'),
+
+('Buzz', 'Apis mellifera', 'Honeybee, vital pollinator', 'Produces honey and wax', 'Insect', 'buzz.jpg', 'Insectarium'),
+
+('Silky', 'Choloepus hoffmanni', 'Two-toed sloth, slow mover', 'Sleeps 15 hours daily', 'Mammal', 'silky.jpg', 'Rainforest Habitat'),
+
+('Mighty', 'Gorilla gorilla', 'Western gorilla, powerful primate', 'Gentle giant', 'Mammal', 'mighty.jpg', 'Primate House'),
+
+('Ivory', 'Ciconia ciconia', 'White stork with long beak', 'Builds huge nests', 'Bird', 'ivory.jpg', 'Wetlands Exhibit'),
+
+('Tango', 'Panthera onca', 'Jaguar, spotted big cat', 'Strong swimmer', 'Mammal', 'tango.jpg', 'Jungle Habitat'),
+
+('Zephyr', 'Vulpes zerda', 'Fennec fox with large ears', 'Adapted to desert life', 'Mammal', 'zephyr.jpg', 'Desert Exhibit'),
+
+('Aurora', 'Phoebetria fusca', 'Sooty albatross with long wingspan', 'Flies long distances', 'Bird', 'aurora.jpg', 'Seabird Aviary'),
+
+('Frost', 'Ursus maritimus', 'Polar bear with white fur', 'Excellent swimmer', 'Mammal', 'frost.jpg', 'Tundra Zone'),
+
+('Piper', 'Cacatua galerita', 'Sulphur-crested cockatoo', 'Mimics human speech', 'Bird', 'piper.jpg', 'Aviary Zone');
