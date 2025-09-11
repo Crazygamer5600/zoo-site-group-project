2af87fcf51a6f0135e66fdb09255506112ee113f
girmy008 <164983725+girmy008@users.noreply.github.com>
2025-09-11 13:35:55 +0930

    SetupDB
    SetupDB

--- ProjectStudioApp/ProjectStudioApp/obj/Debug/net8.0/ProjectStudioApp.AssemblyInfo.cs
+++ ProjectStudioApp/ProjectStudioApp/obj/Debug/net8.0/ProjectStudioApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectStudioApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04ec84f6e8c17e578c7efbc81c6bd801221e9fce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+692a33a6714688fbeb624e5bc299bda87339a8b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectStudioApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectStudioApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
