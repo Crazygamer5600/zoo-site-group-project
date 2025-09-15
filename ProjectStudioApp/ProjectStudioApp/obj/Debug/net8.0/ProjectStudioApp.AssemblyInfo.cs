27a956dcdb0f4f419a7ed602df866ac5e007a7c4
girmy008 <164983725+girmy008@users.noreply.github.com>
2025-09-15 18:04:48 +0930

    image on toolbar

--- ProjectStudioApp/ProjectStudioApp/obj/Debug/net8.0/ProjectStudioApp.AssemblyInfo.cs
+++ ProjectStudioApp/ProjectStudioApp/obj/Debug/net8.0/ProjectStudioApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectStudioApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e48b73292b5ee12ed7176366a065235edf35c8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8a5f0125bff00201022fcecf830ac7e5fb39ac4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectStudioApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectStudioApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
