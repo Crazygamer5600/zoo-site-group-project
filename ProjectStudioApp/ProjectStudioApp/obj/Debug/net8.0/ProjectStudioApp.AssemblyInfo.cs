005e43370ec7bafe63416f8493280019b5efba7d
girmy008 <164983725+girmy008@users.noreply.github.com>
2025-09-15 20:18:10 +0930

    new membership scaffold
    new membership scaffold

--- ProjectStudioApp/ProjectStudioApp/obj/Debug/net8.0/ProjectStudioApp.AssemblyInfo.cs
+++ ProjectStudioApp/ProjectStudioApp/obj/Debug/net8.0/ProjectStudioApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectStudioApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67667946b587b11a604368e11598ae4d776f2794")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1255b5163a5f892cdbf14cc47b4271609cfbe809")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectStudioApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectStudioApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
