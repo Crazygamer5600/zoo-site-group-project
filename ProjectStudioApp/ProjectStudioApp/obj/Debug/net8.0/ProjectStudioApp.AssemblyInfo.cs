ec80f1ee4d72e2924eef02d1b7159ff8336d8de1
girmy008 <164983725+girmy008@users.noreply.github.com>
2025-09-17 17:38:46 +0930

    links to extra pages at bootom of page.
    links to extra pages at bootom of page.

--- ProjectStudioApp/ProjectStudioApp/obj/Debug/net8.0/ProjectStudioApp.AssemblyInfo.cs
+++ ProjectStudioApp/ProjectStudioApp/obj/Debug/net8.0/ProjectStudioApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectStudioApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3de5466917515190a6070a9b5d0c35b46a8002b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c388680a3de907b2467a20c2a478cb567b40a895")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectStudioApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectStudioApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
