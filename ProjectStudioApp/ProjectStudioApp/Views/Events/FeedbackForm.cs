c13a893859b274a0d1c9644ee037527376d303e7
Buwani <167394891+Buwani2004@users.noreply.github.com>
2025-10-07 02:29:13 +1030

    review page

---
+++ ProjectStudioApp/ProjectStudioApp/Views/Events/FeedbackForm.cs
@@ -0,0 +1,6 @@
+﻿namespace ProjectStudioApp.Views.Events
+{
+    public class FeedbackForm
+    {
+    }
+}
---
+++ ProjectStudioApp/ProjectStudioApp/Views/Events/FeedbackForm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectStudioApp.Views.Events
+{
+    public class FeedbackFormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
