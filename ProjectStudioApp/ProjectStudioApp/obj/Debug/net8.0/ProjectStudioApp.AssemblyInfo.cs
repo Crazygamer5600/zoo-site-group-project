589b2858b04024af42b5857d8c2645358287a84f
Buwani <167394891+Buwani2004@users.noreply.github.com>
2025-10-08 04:14:14 +1030

    enforce orange default and grey active across site(navs)

--- ProjectStudioApp/ProjectStudioApp/obj/Debug/net8.0/ProjectStudioApp.AssemblyInfo.cs
+++ ProjectStudioApp/ProjectStudioApp/obj/Debug/net8.0/ProjectStudioApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectStudioApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b9ebcfbe97e32e65a44cbf581a24cd24fc86dd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c32681768016610ea840ed4e4230c55c07e2ee6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectStudioApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectStudioApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
